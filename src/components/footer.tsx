2e4d4271bbe6eabe38aaa8418e7c1a3de74e7a34
rene-ruiz <palaciosruiz.rene@gmail.com>
2024-07-30 14:58:09 -0600

    Initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <div>Main App Footer</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>Main App Navbar</div>
+    </nav>
+  );
+};
+
+export default Navbar;
