6fa6c780e4ddaf65f1839422ea1003cd13b6f5e6
rene-ruiz <palaciosruiz.rene@gmail.com>
2024-07-30 22:56:28 -0600

    Added navbar skeleton

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,31 @@
+"use client";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
-      <div>Main App Navbar</div>
+      <div>
+        <Link href={"/"}>
+          <button>PocketTrack</button>
+        </Link>
+        <div>
+          <ul>
+            <li>
+              <Link href={"/"}>
+                <button>Home</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={"/about"}>
+                <button>About</button>
+              </Link>
+            </li>
+            <li>
+              <button>Login</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
