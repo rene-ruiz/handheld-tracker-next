0316ed40b2950bf7fd2c13b548251f11f91ad0be
rene-ruiz <palaciosruiz.rene@gmail.com>
2024-08-08 18:25:47 -0600

    Added animation to button

--- src/components/buttonComponent.tsx
+++ src/components/buttonComponent.tsx
@@ -1,19 +1,31 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IconFavoriteHeart } from "./iconFavorite";
 import { modifyData } from "@/hooks/useApiActions";
-import { CardHandHeldProps } from "./cardHandHeld";
+import { CardHandHeldProps, HandHeld } from "./cardHandHeld";
 
 export const ButtonComponent: FC<CardHandHeldProps> = ({ handheldData }) => {
   const { id, obtained } = handheldData;
+  const [obtainedFavorite, setObtained] = useState(obtained);
+  const [isAnimating, setIsAnimating] = useState(false);
+
   const handleFavorite = () => {
-    modifyData(id, { obtained: !obtained });
+    modifyData(id, { obtained: !obtainedFavorite }).then((res: HandHeld) => {
+      setObtained(res.obtained);
+    });
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 300);
   };
 
   return (
     <div className="absolute bottom-0 left-0 right-0 flex justify-end p-2">
-      <button onClick={handleFavorite}>
-        <IconFavoriteHeart fill={obtained ? "red" : ""} />
+      <button
+        onClick={handleFavorite}
+        className={`${isAnimating && "animate-ping"}`}
+      >
+        <IconFavoriteHeart fill={obtainedFavorite ? "red" : ""} />
       </button>
     </div>
   );
--- src/components/homeContent.tsx
+++ src/components/homeContent.tsx
@@ -6,7 +6,7 @@ const HomeContent = async () => {
   const handHeldList: HandHeld[] = await useQuery();
   return (
     <div className="container mx-auto p-6 md:px-12 items-center md:items-stretch flex flex-col">
-      <Title content={"Hand held list"} />
+      <Title content={"Hand held game consoles"} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1">
         {handHeldList.map((handheld, index) => (
           <CardHandHeld key={index} handheldData={handheld} />
